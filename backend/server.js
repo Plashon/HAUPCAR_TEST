83ea9e7bb8661a5f6802ba2674e5c8536dd0fa08
plashon <111554614+Plashon@users.noreply.github.com>
2026-07-08 21:46:32 +0700

    connect database

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const cors = require("cors");
+
+const app = require('./src/app');
+const connectDB = require('./src/db');
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+  });
+});
