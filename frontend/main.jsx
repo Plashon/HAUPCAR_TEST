ab684401d884a127b6bcab10cebfd92d36c2d6ae
plashon <111554614+Plashon@users.noreply.github.com>
2026-07-08 21:38:56 +0700

    first commit

---
+++ frontend/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import router from './routes/Router';
+import { RouterProvider } from 'react-router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+    <ToastContainer
+      position="top-center"
+      autoClose={1500}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+  </StrictMode>
+);
